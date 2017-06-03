d4071deb6dac5b45fe54f3b7f9fa53f4fafa6d8f
KwatME <kwat.medetgul.ernar@gmail.com>
2017-06-03 00:32:28 -0700

    CAF

--- variant/vcf.py
+++ variant/vcf.py
@@ -274,5 +274,5 @@ def get_vcf_population_allelic_frequencies(caf):
     try:
         return [float(a_caf) for a_caf in caf.split(',')]
     except ValueError:
-        print('Bad CAF {}.'.format(caf))
-        return [float(a_caf) for a_caf in caf.strip('.').strip(',').split(',')]
+        print('Strange CAF {}.'.format(caf))
+        return [float(a_caf) for a_caf in caf.split(',') if a_caf != '.']
